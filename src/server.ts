8f9f9e41e6a421fa11993b37777ab699ef731381
Cassiano26 <candidocassiano2@gmail.com>
2025-02-26 14:57:15 -0300

    feat: add fastify plugins

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
 import fastify from 'fastify'
-import { knex } from './database'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
