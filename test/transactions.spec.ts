311887848ce35897d489d97cb7d01c3f7356ea59
Cassiano26 <candidocassiano2@gmail.com>
2025-02-27 15:50:39 -0300

    feat: add list transaction and summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -52,4 +52,63 @@ describe('Transactions routes', () => {
       }),
     ])
   })
+
+  it('should be able to list one specific user transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies!)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const listSpecificTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies!)
+
+    expect(listSpecificTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('should be able to list the user transactions summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies!)
+      .send({
+        title: 'Second transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const listSummaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies!)
+      .expect(200)
+
+    expect(listSummaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
 })
